13ffa3a48763dd09ce650a0e39eed80b203dd6f5
clairenakilacc <clairenakilacc@gmail.com>
2025-10-22 13:58:13 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ignores ESLint errors/warnings on Vercel builds
+  },
 };
 
 export default nextConfig;
