217e3af09df7f7d5d4672de3c53bf234c6fdf7d6
Gitmar2 <jhomarvaron16@gmail.com>
2026-01-13 19:50:36 +0800

    update spacing cards

--- app/suemishop/profile/list/page.tsx
+++ app/suemishop/profile/list/page.tsx
@@ -5,10 +5,10 @@ import PasswordSettings from "../../../components/profile/PasswordSettings";
 
 export default function ProfilePage() {
   return (
-    <div className="flex gap-6 p-6 bg-[#fff9f9] min-h-screen">
+    <div className="flex w-full gap-6 p-6 bg-[#fff9f9] min-h-screen">
       <ProfileCard />
 
-      <div className="flex flex-col gap-6 w-full max-w-3xl">
+      <div className="flex flex-col gap-6 flex-1">
         <ProfileSettings />
         <PasswordSettings />
       </div>
